dda6b9947ffc770a24b568c74f06943569ab0f49
xuchen <xu.chen@grabtaxi.com>
2019-03-31 16:31:51 +0800

    add dictionary to store user action

--- server/app.py
+++ server/app.py
@@ -12,8 +12,21 @@ app = Flask(__name__)
 
 
 @app.route("/api/ask", methods=["POST"])
-def answer():
+def ask():
+    """
+    {
+        "question": "string",
+        "user_id": "string",
+    }
+    :return:
+    """
     data = request.get_json()
+
+    if data['user_id'] not in input_dict:
+        return "this user does not have any action yet"
+
+    input_dict[data['user_id']].append(data['question'])
+
     if model:
         ans = model.predict(data)
         prediction = np.array2string(ans)
@@ -21,6 +34,30 @@ def answer():
         prediction = {"error": "no module is available"}
     resp = jsonify(prediction)
     return resp
+
+
+@app.route("/api/action", methods=["POST"])
+def input_action():
+    """
+    {
+        "action": "left|right|up|down",
+        "user_id": "string",
+    }
+    :return:
+    """
+    # frontend post user actions in this api, actions are the input of input model
+
+    data = request.get_json()
+
+    # save action into dict and later will be sent to model along with question
+    if data['user_id'] in input_dict:
+        input_dict[data['user_id']].append(data['action'])
+    else:
+        input_dict[data['user_id']] = [data['action']]
+
+    print(input_dict)
+
+    return "successfully update"
 
 
 @app.route("/", defaults={"path": ""})
@@ -33,6 +70,7 @@ def load_model():
     try:
         model_file = "../model/model.pickle"
         model = p.load(open(model_file, "rb"))
+
         return model
     except:
         return None
@@ -40,4 +78,9 @@ def load_model():
 
 if __name__ == "__main__":
     model = load_model()
+
+    # user_id -> input data list
+    input_dict = {
+        "dummy_id": ["data", "question"]
+    }
     app.run(debug=True, host="0.0.0.0")
