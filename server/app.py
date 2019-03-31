9c03378b489333281e73f4039a1d05fa876bee03
Yuanda <yuanda@u.nus.edu>
2019-03-31 15:12:39 +0800

    basic flask server v2

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ Reference:
 https://www.kdnuggets.com/2019/01/build-api-machine-learning-model-using-flask.html
 """
 
-from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, jsonify
 import numpy as np
 import pickle as p
 import json
@@ -11,14 +11,33 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/api/talk', methods=['POST'])
-def makecalc():
+@app.route("/api/ask", methods=["POST"])
+def answer():
     data = request.get_json()
-    prediction = np.array2string(model.predict(data))
-    return jsonify(prediction)
+    if model:
+        ans = model.predict(data)
+        prediction = np.array2string(ans)
+    else:
+        prediction = {"error": "no module is available"}
+    resp = jsonify(prediction)
+    return resp
 
 
-if __name__ == '__main__':
-    model_file = '../model/model.pickle'
-    model = p.load(open(model_file, 'rb'))
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def default_answer(path):
+    return "this address {} is not available".format(path)
+
+
+def load_model():
+    try:
+        model_file = "../model/model.pickle"
+        model = p.load(open(model_file, "rb"))
+        return model
+    except:
+        return None
+
+
+if __name__ == "__main__":
+    model = load_model()
+    app.run(debug=True, host="0.0.0.0")
