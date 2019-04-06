e25c37343804a2e72da5849e2865361c10504bf5
xuchen <xu.chen@grabtaxi.com>
2019-04-06 16:56:50 +0800

    commit for merge

--- server/app.py
+++ server/app.py
@@ -7,6 +7,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
 import pickle as p
+import torch
+import argparse
+import pickle
+import pprint
+
+
+
+from model.model import DMN
 
 app = Flask(__name__)
 CORS(app)
@@ -70,17 +78,83 @@ def default_answer(path):
 
 def load_model():
     try:
-        model_file = "../model/model.pickle"
-        model = p.load(open(model_file, "rb"))
+        # model_file = "../model/model.pickle"
+        # model = p.load(open(model_file, "rb"))
 
-        return model
+        print("-------- here1")
+        args = get_model_args()
+        print("-------- here2")
+        dataset = pickle.load(open(args.data_path, 'rb'))
+        print("-------- here3")
+        dataset.config.__dict__.update(args.__dict__)
+        print("-------- here4")
+        args.__dict__.update(dataset.config.__dict__)
+        print("-------- here5")
+        pp = lambda x: pprint.PrettyPrinter().pprint(x)
+        pp(args.__dict__)
+        print("-------- here6")
+
+        USE_CUDA = torch.cuda.is_available()
+        device = torch.device("cuda" if USE_CUDA else "cpu")
+
+        m = DMN(args, dataset.idx2vec, args.set_num).to(device)
+        m.load_checkpoint()
+        m.eval()
+        print("-------- here7")
+
+        return m
     except:
+        print("exception")
         return None
 
 
-if __name__ == "__main__":
-    model = load_model()
+def get_model_args():
+    argparser = argparse.ArgumentParser()
+    # run settings
+    argparser.add_argument('--data_path', type=str, default='./data/babi(tmp).pkl')
+    argparser.add_argument('--model_name', type=str, default='m')
+    argparser.add_argument('--checkpoint_dir', type=str, default='./results/')
+    argparser.add_argument('--batch_size', type=int, default=32)
+    argparser.add_argument('--epoch', type=int, default=100)
+    argparser.add_argument('--train', type=int, default=0)
+    argparser.add_argument('--valid', type=int, default=0)
+    argparser.add_argument('--test', type=int, default=1)
+    argparser.add_argument('--early_stop', type=int, default=0)
+    argparser.add_argument('--resume', action='store_true', default=False)
+    argparser.add_argument('--save', action='store_true', default=False)
+    argparser.add_argument('--print_step', type=float, default=128)
 
+    # model hyperparameters
+    argparser.add_argument('--lr', type=float, default=0.0003)
+    argparser.add_argument('--lr_decay', type=float, default=1.0)
+    argparser.add_argument('--wd', type=float, default=0)
+    argparser.add_argument('--grad_max_norm', type=int, default=5)
+    argparser.add_argument('--s_rnn_hdim', type=int, default=100)
+    argparser.add_argument('--s_rnn_ln', type=int, default=1)
+    argparser.add_argument('--s_rnn_dr', type=float, default=0.0)
+    argparser.add_argument('--q_rnn_hdim', type=int, default=100)
+    argparser.add_argument('--q_rnn_ln', type=int, default=1)
+    argparser.add_argument('--q_rnn_dr', type=float, default=0.0)
+    argparser.add_argument('--e_cell_hdim', type=int, default=100)
+    argparser.add_argument('--m_cell_hdim', type=int, default=100)
+    argparser.add_argument('--a_cell_hdim', type=int, default=100)
+    argparser.add_argument('--word_dr', type=float, default=0.2)
+    argparser.add_argument('--g1_dim', type=int, default=500)
+    argparser.add_argument('--max_episode', type=int, default=10)
+    argparser.add_argument('--beta_cnt', type=int, default=10)
+    argparser.add_argument('--set_num', type=int, default=1)
+    argparser.add_argument('--max_alen', type=int, default=2)
+    args = argparser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    print("model loading ...")
+    model = load_model()
+    print("model loaded ...")
+
+    # model.
     # user_id -> input data list
     input_dict = {
         "dummy_id": ["data", "question"]
